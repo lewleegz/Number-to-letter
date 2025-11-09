fce1e7e24dae428c53804c4faff47ed98ea38e31
Lee Goldin Zawieja <lewleegz@gmail.com>
2025-11-09 10:00:53 +0100

    Update Number to letter.py

--- Number to letter.py
+++ Number to letter.py
@@ -4,10 +4,10 @@ letter_exists = False
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 p = inflect.engine()
 number = st.text_input("Input your number:", "1234")
+number_in_letters = number
 for c in letters:
   if c in number:
     letter_exists = True
-    number_in_letters = number
 if not letter_exists:
   try:
     number_in_letters = p.number_to_words(number)
