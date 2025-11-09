5555052dff1c706a1ab1e4fd2050bb4f656a402e
Lee Goldin Zawieja <lewleegz@gmail.com>
2025-11-09 09:57:55 +0100

    Update Number to letter.py

--- Number to letter.py
+++ Number to letter.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import inflect
+letter_exists = False
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 p = inflect.engine()
 number = st.text_input("Input your number:", "1234")
