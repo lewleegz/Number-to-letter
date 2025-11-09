5715113a7e6d5787e53a0d4c80cba04ef10ef545
Lee Goldin Zawieja <lewleegz@gmail.com>
2025-11-09 09:57:03 +0100

    Implement letter check before number conversion
    Add letter existence check before converting number

--- Number to letter.py
+++ Number to letter.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import inflect
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 p = inflect.engine()
 number = st.text_input("Input your number:", "1234")
-number_in_letters = p.number_to_words(number)
+for c in letters:
+  if c in number:
+    letter_exists = True
+if not letter_exists:
+  try:
+    number_in_letters = p.number_to_words(number)
+  except Exception:
+    pass
 st.write(f"The number is {number_in_letters} in letters")
 st.write("Thanks for the idea Isidor!")
